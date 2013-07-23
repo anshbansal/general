4ac6df8fe4ff547f9ef0928c3b0006f17482c984
anshbansal <aseembansal@ymail.com>
2013-07-23 23:26:09 +0530

    Ported 4 project euler solutions to Python3

--- Python27/project_euler/001_050/029.py
+++
@@ -1,7 +0,0 @@
-lisst = set()
-
-for a in xrange(2,101):
-    for b in xrange(2,101):
-        lisst.update([pow(a,b)])
-
-print len(lisst)
---
+++ Python33/project_euler/001_050/025.py
@@ -0,0 +1,12 @@
+from itertools import count
+
+def prob_025():
+    a, b, digits = 1, 1, 1
+    for number in count(2):
+        if digits > 999:
+            return number
+        a, b = b, a + b
+        digits = len(str(b))
+
+if  __name__ == "__main__":
+    print(prob_025())

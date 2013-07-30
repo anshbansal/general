57efa9a8e040421c0c9df5e77204d1c65e5578cc
anshbansal <aseembansal@ymail.com>
2013-07-31 00:21:09 +0530

    Updated stack implementation by using arrays. Much more readable and maintainable.

--- C/Data_Structures/Stack/stack_by_array.c
+++ C/Data_Structures/Stack/stack_by_array.c
@@ -1,88 +1,36 @@
+#include<assert.h>
+#include<errno.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include<errno.h>
 
 static const int MAX_SIZE = 100;
-static const int MIN = 1;
-static const int MAX = 4;
+enum action {PUSH = 1, POP, TOP, QUIT};
 
-void intro();
-
-void push(int *, int *, int);
-int pop(int *, int *);
-int top(int *, int *);
-
-int main()
+void clear_screen(void)
 {
-    int arr[MAX_SIZE];
-    int length = 0;
-
-    for (;;)
-    {
-        int choice = MAX + 1;
-        while (MAX < choice || choice < MIN)
-        {
-            system("cls");
-            intro();
-            printf("Enter your choice -> ");
-            scanf("%d", &choice);
-        }
-
-        system("cls");
-        int data;
-        errno = 0;
-        switch (choice)
-        {
-        case 1:
-            printf("\nEnter data to be pushed -> ");
-            scanf("%d", &data);
-            push(arr, &length, data);
-            if (errno == 1){
-                printf("\nStack overflow");
-            }
-            break;
-        case 2:
-            data = pop(arr, &length);
-            if (errno == 2){
-                printf("\nStack underflow");
-            }
-            else{
-                printf("\nThe data is %d", data);
-            }
-            break;
-        case 3:
-            data = top(arr, &length);
-            if (errno == 1){
-                printf("\nStack overflow");
-            }
-            else if (errno == 2){
-                printf("\nStack underflow");
-            }
-            else{
-                printf("\nThe data at top is %d", data);
-            }
-            break;
-        case 4:
-            return 0;
-        }
-        printf("\nLength is %d", length);
-        getchar();
-        getchar();
-    }
+    system("cls");
 }
 
-void intro()
+static enum action get_user_action(void)
 {
-    printf("1 Push data\n");
-    printf("2 Pop Data\n");
-    printf("3 See the top of the stack\n");
-    printf("4 Exit this program\n\n");
+    int choice = 0;
+    do
+    {
+        clear_screen();
+        printf("%d Push data\n"
+               "%d Pop Data\n"
+               "%d See the top of the stack\n"
+               "%d Exit\n\n"
+               "Enter your choice -> ", PUSH, POP, TOP, QUIT);
+        scanf("%d", &choice);
+    } while (choice != PUSH && choice != POP && choice != TOP && choice != QUIT);
+    return (enum action) choice;
 }
 
 void push(int *arr, int *length, int data)
 {
     if (*length == MAX_SIZE){
-        errno = 1;
+        errno = PUSH;
         return;
     }
     arr[(*length)++] = data;
@@ -91,7 +39,7 @@ void push(int *arr, int *length, int data)
 int pop(int *arr, int *length)
 {
     if (*length == 0){
-        errno = 2;
+        errno = POP;
         return -1;
     }
     return arr[--(*length)];
@@ -100,12 +48,67 @@ int pop(int *arr, int *length)
 int top(int *arr, int *length)
 {
     if (*length == 0){
-        errno = 2;
+        errno = POP;
         return -1;
     }
     else if (*length == MAX_SIZE){
-        errno = 1;
+        errno = PUSH;
         return -1;
     }
     return arr[*length - 1];
+}
+
+int main(void)
+{
+    int arr[MAX_SIZE];
+    int length = 0;
+
+    enum action choice;
+    while ((choice = get_user_action()) != QUIT)
+    {
+        clear_screen();
+        int data;
+        errno = 0;
+
+        switch (choice)
+        {
+        case PUSH:
+            printf("Enter data to be pushed -> ");
+            scanf("%d", &data);
+            push(arr, &length, data);
+            if (errno == PUSH){
+                printf("Stack overflow\n");
+            }
+            break;
+        case POP:
+            data = pop(arr, &length);
+            if (errno == POP){
+                printf("Stack underflow\n");
+            }
+            else{
+                printf("The data is %d\n", data);
+            }
+            break;
+        case TOP:
+            data = top(arr, &length);
+            switch (errno)
+            {
+            case PUSH:
+                printf("Stack overflow\n");
+                break;
+            case POP:
+                printf("Stack underflow\n");
+                break;
+            default:
+                printf("The data at top is %d\n", data);
+            }
+            break;
+        default:
+            assert(!"You should not have reached this.");
+        }
+
+        printf("Length is %d\n", length);
+        getchar();
+        getchar();
+    }
 }

878a018bf8fa7e31076fccfde71366469a236324
anshbansal <aseembansal@ymail.com>
2013-08-01 12:54:25 +0530

    Changed project euler solution for clarity

--- Python33/project_euler/001_050/019.py
+++ Python33/project_euler/001_050/019.py
@@ -1,15 +1,24 @@
+def is_leap_year(year):
+    return (year % 4 == 0) and ( \
+        (year % 100 != 0) or (year % 400 == 0))
+
 def prob_019():
     total = 0
-    days = 0
-    for year in range(1900, 2001):
+    days = 1 + 365
+    #1 for monday
+    #Added 365 because 1900 isn't a leap year
+    for year in range(1901, 2001):
+        is_leap = is_leap_year(year)
         for month in range(1, 13):
             if month in [4, 6, 9, 11]:
                 days += 30
             elif month == 2:
-                if year % 4: # Not leap year
-                    days += 28
-                if not(year % 4) and year % 400: #Leap year
+                if is_leap:
                     days += 29
+                else:
+                    days += 28
+            else:
+                days += 31
 
             days %= 7
             if days == 0:

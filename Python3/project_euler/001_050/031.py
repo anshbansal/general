6ffd6d4b8649f3127d3e6241acac2ee15bd816ec
anshbansal <aseembansal@live.com>
2014-04-12 00:50:56 +0530

    Changed a bit to make it easier to refactor

--- Python3/project_euler/001_050/031.py
+++ Python3/project_euler/001_050/031.py
@@ -2,33 +2,32 @@ __author__ = 'Aseem'
 
 
 def prob_031():
-    range_value = [(3, 100), ()]
-    #TODO Took 1.407 seconds
+    #TODO Optimize Took 1.407 seconds
     ways = 1
-    for i in range(3): #For 100
-        sum_ = i * 100
-        for j in range(5): #for 50
-            sum_a = sum_ + j*50
-            if sum_a > 200:
+    for i in range(3):
+        sum_a = i * 100
+        for j in range(5):
+            sum_b = sum_a + j*50
+            if sum_b > 200:
                 break
-            for k in range(11): #For 20
-                sum_b = sum_a + k*20
-                if sum_b > 200:
+            for k in range(11):
+                sum_c = sum_b + k*20
+                if sum_c > 200:
                     break
-                for l in range(21): #For 10
-                    sum_c = sum_b + l*10
-                    if sum_c > 200:
+                for l in range(21):
+                    sum_d = sum_c + l*10
+                    if sum_d > 200:
                         break
-                    for m in range(41): #For 5
-                        sum_d = sum_c + m*5
-                        if sum_d > 200:
+                    for m in range(41):
+                        sum_e = sum_d + m*5
+                        if sum_e > 200:
                             break
-                        for n in range(101): #for 2
-                            sum_e = sum_d + n*2
-                            if sum_e > 200:
+                        for n in range(101):
+                            sum_f = sum_e + n*2
+                            if sum_f > 200:
                                 break
-                            for o in range(201): #For 1
-                                if sum_e + o == 200:
+                            for o in range(201):
+                                if sum_f + o == 200:
                                     ways += 1
 
     return ways

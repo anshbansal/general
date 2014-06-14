925d9b72aff962dafeda2b000dca43c6715fadbc
anshbansal <aseembansal@live.com>
2014-06-14 23:28:18 +0530

    Old change for making code clear. Made changes for filtering the directories. Made traversal much faster

--- Python3/Automation/zip_sets.py
+++ Python3/Automation/zip_sets.py
@@ -18,9 +18,8 @@ def folder_sets(dir_names):
 
 def make_zips(dir_path, dir_names):
     for folders in folder_sets(dir_names):
-        start_num = folders[0][:FOLDER_NUM_LEN]
-        archive_name = start_num + "-" + str(int(start_num) + FOLDER_SET - 1).zfill(FOLDER_NUM_LEN) + ".zip"
-        with zipfile.ZipFile(os.path.join(dir_path, archive_name), "w", zipfile.ZIP_DEFLATED) as z:
+        archive_name = os.path.join(dir_path, folders[0][:FOLDER_NUM_LEN] + "-" + folders[-1][:FOLDER_NUM_LEN] + ".zip")
+        with zipfile.ZipFile(archive_name, "w", zipfile.ZIP_DEFLATED) as z:
             for folder in folders:
                 for i in os.listdir(os.path.join(dir_path, folder)):
                     z.write(os.path.join(dir_path, folder, i), os.path.join(folder, i))
@@ -28,9 +27,15 @@ def make_zips(dir_path, dir_names):
                 for folder in folders:
                     shutil.rmtree(os.path.join(dir_path, folder))
 
+def filter_directories(dir_names):
+    for directory in dir_names:
+        if directory.endswith("_Read"):
+            return [directory]
+    return dir_names
 
 def main():
     for dir_path, dir_names, file_names in os.walk('.'):
+        dir_names[:] = filter_directories(dir_names)
         if dir_path.endswith("_Read"):
             make_zips(dir_path, dir_names)
 

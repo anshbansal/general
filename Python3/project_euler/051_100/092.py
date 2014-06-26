1843cefc15e4b4b2be5a72e4c48fb6720d2bf4e1
anshbansal <aseembansal@live.com>
2014-06-27 00:05:21 +0530

    Updated project euler solution

--- Python3/project_euler/051_100/092.py
+++ Python3/project_euler/051_100/092.py
@@ -11,28 +11,31 @@ def sum_of_squares_of_digits(num):
     return ans
 
 
+def set_all(ends, members, value):
+    for j in members:
+        ends[j] = value
+
+
 def prob_092():
-    #TODO Maybe Optimize - 59.801 sec
+    # TODO Maybe Optimize - 53.801 sec
     ends = [0] * 10000001
-    for i in range(1, 10000000):
-        members = set([i])
-        q = members.update
-        temp = i
-        while not ends[temp] and (temp - 89) and (temp - 1):
-            temp = sum_of_squares_of_digits(temp)
-            q([temp])
-
+    for temp in range(1, 10000000):
         if ends[temp]:
-            for j in members:
-                ends[j] = ends[temp]
+            continue
+        members = {temp}
+        while (temp - 89) and (temp - 1):
+            temp = sum_of_squares_of_digits(temp)
+            members.add(temp)
+            if ends[temp]:
+                set_all(ends, members, ends[temp])
+                break
         else:
-            for j in members:
-                ends[j] = temp
+            set_all(ends, members, temp)
 
-    return Counter(ends)
+    return Counter(ends)[89]
+
 
 if __name__ == "__main__":
-    import time
-    t = time.time()
-    print(prob_092())
-    print(time.time() - t)
+    from project_euler import common
+
+    common.run_all(__name__)
